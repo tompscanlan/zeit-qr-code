cb49dcb07e80b4d4080ceb73f98d77635833f950
Tom Scanlan <tscanlan@vmware.com>
2019-12-21 16:49:47 -0500

    add more

---
+++ api/qrcode/go.mod
@@ -0,0 +1,3 @@
+module qrcode
+
+require github.com/skip2/go-qrcode v0.0.0-20191027152451-9434209cb086
---
+++ api/qrcode/index.go
@@ -0,0 +1,10 @@
+package qrcode
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
+}
