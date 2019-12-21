cba87d12fb7550cccd3408a927687a47dc99abd0
Tom Scanlan <tscanlan@vmware.com>
2019-12-21 17:20:25 -0500

    add qr

---
+++ api/qrcode/qr.go
@@ -0,0 +1,29 @@
+package zeit_qr_code
+
+import (
+	// "fmt"
+	"net/http"
+	"strconv"
+
+	qc "github.com/skip2/go-qrcode"
+)
+
+// Handler asd
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var image []byte
+	image, err := qc.Encode("https://example.org", qc.Medium, 256)
+
+	if err != nil {
+		_ = image
+	}
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Content-Length", strconv.Itoa(len(image)))
+	if _, err := w.Write(image); err != nil {
+		http.Error(w, "failed to write image", http.StatusPartialContent)
+	}
+
+	// if _, err := w.Write(buffer.Bytes()); err != nil {
+	// log.Println("unable to write image.")
+	// }
+	// fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module zeit_qr_code
 
 go 1.13
 
-require github.com/skip2/go-qrcode v0.0.0-20191027152451-9434209cb086 // indirect
+require github.com/skip2/go-qrcode v0.0.0-20191027152451-9434209cb086
